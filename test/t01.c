6527862e8d615c7c399da6a3d875a2320eab6b93
PengDu2007 <peng_du2007@qq.com>
2020-08-03 23:45:33 +0800

    update

--- test/t01.c
+++ test/t01.c
@@ -2,20 +2,25 @@
 // 输入10名学生成绩，求其中最大值，最小值，平均值。
 
 int t01() {
+    // 1. 定义变量
     int scores[10];
+    int sum = 0;
+    double average;
+
+    // 输入数据, 90 100 50 30 20 40 80 60 70 10
     printf("Please input the scores of 10 students: ");
     for ( int i = 0; i < 10; i++) {
         scanf("%d", &scores[i]);
     }
+
+    // 打印输入数据
     printf("inputs: \n");
     for (int i = 0; i < 10; i++) {
         printf("%d ", scores[i]);
     }
     printf("\n");
 
-    int sum = 0;
-    double average;
-
+    // 排序, 找出最大值和最小值以及累加总和
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10 - i - 1; j++) {
             if ( scores[j] > scores[j+1]) {
