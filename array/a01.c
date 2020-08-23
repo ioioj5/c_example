eb95dee2e959730d9b95a2f099662f568c59c929
PengDu2007 <peng_du2007@qq.com>
2020-08-23 22:23:56 +0800

    point

--- array/a01.c
+++ array/a01.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+void swap(int *p1, int *p2);
+void swap2(int *p1, int *p2);
 
 int a01() {
-    char a[8], b[8];
-    scanf("%s", a);
-//    for (int i = 0; i < 5; i++) {
-//        scanf("%c", &a[i]);
-//    }
+//    int a[10] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90};
+//    int *p;
+//    p = a;
+//
+//    printf("a[0] = %d, a[2] = %d\n",*p, *(p+2));
 
-//    for (int i = 0; i < 8; i++) {
-//        printf("%c", a[i]);
-//    }
-    printf("%s", a);
+    int a = 3, b = 5;
+    int *p_1, *p_2;
+    p_1 = &a;
+    p_2 = &b;
+    swap2(p_1, p_2);
+    printf("a = %d, b = %d\n", a, b);
     return 0;
+}
+// 地址交换
+void swap(int *p1, int *p2) {
+    int *temp;
+    temp = p1;
+    p1 = p2;
+    p2 = temp;
+}
+// 值交换
+void swap2(int *p1, int *p2) {
+    int temp;
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
 }
